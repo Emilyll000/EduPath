f83fedd7a8183e2818bec7ab3883a250c2345be5
Alex Hernandez <133897289+AlexHernandez2698632494@users.noreply.github.com>
2026-04-11 10:29:56 -0600

    Update MateriaPensum.kt

--- app/src/main/java/com/example/edupath_invest/MateriaPensum.kt
+++ app/src/main/java/com/example/edupath_invest/MateriaPensum.kt
@@ -1,10 +1,15 @@
 package com.example.edupath_invest
 
 data class MateriaPensum(
+    val correlativo: Int = 0,
     val codigo: String,
     val nombre: String,
     val ciclo: Int,
     val anioPensum: Int,
+    val anioEtiqueta: String = "",
+    val cicloEtiqueta: String = "",
+    val prerequisitoEtiqueta: String = "",
+    val unidadesValorativas: Int = 0,
     val prerequisitos: List<String> = emptyList(),
     var estado: EstadoMateria = EstadoMateria.PENDIENTE
 )
